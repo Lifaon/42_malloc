40902c0a221463a433b1ea275e313494b47a9df4
Meriadec LANTONNET <mlantonn@student.42.fr>
2019-10-18 15:27:30 +0200

    free now always keeps one empty zone available

--- src/free.c
+++ src/free.c
@@ -6,14 +6,14 @@
 /*   By: mlantonn <mlantonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 17:36:00 by mlantonn          #+#    #+#             */
-/*   Updated: 2019/10/17 16:59:43 by mlantonn         ###   ########.fr       */
+/*   Updated: 2019/10/18 15:25:04 by mlantonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sys/mman.h>
 #include "mymalloc.h"
 
-t_zone	*match_ptr(t_zone *zone, void *ptr, int *i)
+t_zone		*match_ptr(t_zone *zone, void *ptr, int *i)
 {
 	t_zone	*tmp;
 	size_t	coeff;
@@ -38,7 +38,7 @@ t_zone	*match_ptr(t_zone *zone, void *ptr, int *i)
 	return (NULL);
 }
 
-void	match_zone_ptr(t_zone **zone, void *ptr, int *i)
+void		match_zone_ptr(t_zone **zone, void *ptr, int *i)
 {
 	*i = 0;
 	*zone = match_ptr(g_data.tiny, ptr, i);
@@ -48,30 +48,48 @@ void	match_zone_ptr(t_zone **zone, void *ptr, int *i)
 		*zone = match_ptr(g_data.large, ptr, i);
 }
 
-void	free_area(t_zone *zone, t_zone **g_data_ptr, int i)
+static void	free_zone(t_zone *zone, t_zone **g_data_ptr)
 {
+	munmap(zone->ptr, zone->size);
+	if (zone == zone->next)
+		*g_data_ptr = NULL;
+	else
+	{
+		if (zone->is_root)
+		{
+			zone->next->is_root = 1;
+			*g_data_ptr = zone->next;
+		}
+		zone->prev->next = zone->next;
+		zone->next->prev = zone->prev;
+	}
+	munmap(zone, sizeof(t_zone));
+}
+
+void		free_area(t_zone *zone, t_zone **g_data_ptr, int i)
+{
+	t_zone	*tmp;
+
+	if (zone->kind == LARGE)
+	{
+		free_zone(zone, g_data_ptr);
+		return ;
+	}
 	zone->allocated[i] = 0;
 	zone->areas_left++;
 	if (zone->areas_left == zone->limit)
 	{
-		munmap(zone->ptr, zone->size);
-		if (zone == zone->next)
-			*g_data_ptr = NULL;
-		else
-		{
-			if (zone->is_root)
+		tmp = zone;
+		while ((tmp = tmp->next) != zone)
+			if (tmp->areas_left == tmp->limit)
 			{
-				zone->next->is_root = 1;
-				*g_data_ptr = zone->next;
+				free_zone(zone, g_data_ptr);
+				return ;
 			}
-			zone->prev->next = zone->next;
-			zone->next->prev = zone->prev;
-		}
-		munmap(zone, sizeof(t_zone));
 	}
 }
 
-void	free(void *ptr)
+void		free(void *ptr)
 {
 	t_zone	*zone;
 	int		i;
